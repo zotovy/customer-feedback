100b0607d3cd0fbfd2ae06ca3acdf2d3f39a2288
Yaroslav Zotov <35657233+zotovy@users.noreply.github.com>
2022-05-10 18:34:42 +0500

    feat: implement adding feedback

--- go.mod
+++ go.mod
@@ -2,4 +2,13 @@ module customer-feedback
 
 go 1.18
 
-require github.com/lib/pq v1.10.5 // indirect
+require (
+	github.com/andybalholm/brotli v1.0.4 // indirect
+	github.com/gofiber/fiber/v2 v2.33.0 // indirect
+	github.com/klauspost/compress v1.15.0 // indirect
+	github.com/lib/pq v1.10.5 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.35.0 // indirect
+	github.com/valyala/tcplisten v1.0.0 // indirect
+	golang.org/x/sys v0.0.0-20220227234510-4e6760a101f9 // indirect
+)
--- main.go
+++ main.go
@@ -1,12 +1,81 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"github.com/gofiber/fiber/v2"
+	_ "github.com/lib/pq"
 	"log"
 	"os"
+	"strconv"
 )
 
+type Feedback struct {
+	ID     string `json:"id"`
+	Email  string `json:"email"`
+	Date   string `json:"date"`
+	Source string `json:"source"`
+}
+
+// Database instance
+var db *sql.DB
+
+// Database settings
+var (
+	host     = os.Getenv("DATABASE_HOST")
+	port, _  = strconv.Atoi(os.Getenv("DATABASE_PORT"))
+	user     = os.Getenv("POSTGRES_USER")
+	password = os.Getenv("POSTGRES_PASSWORD")
+	dbname   = os.Getenv("POSTGRES_DB")
+)
+
+func Connect() error {
+	fmt.Println(fmt.Sprintf("postgres://%v:%v@%v:%v/%v?sslmode=disable", user, password, host, port, dbname))
+
+	var err error
+	db, err = sql.Open("postgres", fmt.Sprintf("postgres://%v:%v@%v:%v/%v?sslmode=disable", user, password, host, port, dbname))
+	if err != nil {
+		return err
+	}
+	if err = db.Ping(); err != nil {
+		return err
+	}
+	return nil
+}
+
 func main() {
-	log.SetOutput(os.Stdout)
-	fmt.Println("Hello world!")
+	// Connect with database
+	if err := Connect(); err != nil {
+		log.Fatal(err)
+	}
+
+	// Create a Fiber app
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Status(201).JSON("Success")
+	})
+
+	// Create new Feedback
+	app.Post("/add", func(c *fiber.Ctx) error {
+		u := new(Feedback)
+
+		// Parse body into struct
+		if err := c.BodyParser(u); err != nil {
+			return c.Status(400).SendString(err.Error())
+		}
+
+		// Insert Feedback into database
+		_, err := db.Query("INSERT INTO FEEDBACK (email, Source) VALUES ($1, $2)", u.Email, u.Source)
+		if err != nil {
+			return err
+		}
+
+		// Print result
+		log.Println(fmt.Sprintf("Insert %s", u.Email))
+
+		return c.Status(201).JSON(&fiber.Map{"success": true})
+	})
+
+	app.Listen(":8080")
 }
